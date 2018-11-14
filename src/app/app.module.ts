64011f5d5131a4bf811c49d657e775ac138ad0f7
Liam Bayly <me@raybayly.com>
2018-11-14 14:14:08 -0500

    update the bloc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
+import {  HttpModule } from '@angular/http';
+import { HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { GatewayInterceptor } from './core/interceptors/error.interceptor';
-
+import { MaterialModule } from './core/modules/material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   entryComponents: [AppComponent],
